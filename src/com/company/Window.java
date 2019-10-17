192c86bcdfd7a1be6d24708105e5243f51962efe
Vendoros <Good0908>
2019-10-18 01:07:27 +0800

    0.0.3 create config class and commented window class

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -4,20 +4,20 @@ import javax.swing.*;
 
 public class Window implements Runnable {
 
-    JFrame frame;
+    private JFrame frame;
 
     @Override
     public void run() {
         initFrame();
     }
 
-    void initFrame(){
-        frame = new JFrame();
-        frame.getContentPane().setLayout(null);
-        frame.setSize(400,400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.setTitle("Life Game");
+    private void initFrame() {
+        frame = new JFrame();//создаем фрэйи
+        frame.getContentPane().setLayout(null);//расположение на всю форму
+        frame.setSize(Config.SIZE * Config.WIDTH, Config.SIZE * Config.HEIGHT);// устанавливаем размеры окна
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//выход из программы при закрытии
+        frame.setLocationRelativeTo(null);//
+        frame.setVisible(true);// делаем окно видимым
+        frame.setTitle("Life Game");//задаем заголовок окна
     }
 }
